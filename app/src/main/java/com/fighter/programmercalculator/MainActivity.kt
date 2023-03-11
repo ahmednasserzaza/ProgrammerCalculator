6022a467cae2d736bc6e266d94bc96a384ad999d
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-03-11 17:07:27 +0200

    create repository for programmer calculator

---
+++ app/src/main/java/com/fighter/programmercalculator/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.fighter.programmercalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import com.fighter.programmercalculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var converter: Converter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setup()
+    }
+
+    private fun setup() {
+        initSpinners()
+        converter = Converter()
+        binding.buttonGetResult.setOnClickListener {
+            convert()
+        }
+    }
+
+    private fun initSpinners() {
+        binding.spinnerFrom.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when (position) {
+                    0 -> binding.editTextEntering.hint = getString(R.string.binary_hint)
+                    1 -> binding.editTextEntering.hint = getString(R.string.decimal_hint)
+                    2 -> binding.editTextEntering.hint = getString(R.string.octal_hint)
+                    3 -> binding.editTextEntering.hint = getString(R.string.hexadecimal_hint)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+        binding.spinnerTo.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when (position) {
+                    0 -> binding.resultHeader.text = getString(R.string.binary_header)
+                    1 -> binding.resultHeader.text = getString(R.string.decimal_header)
+                    2 -> binding.resultHeader.text = getString(R.string.octal_header)
+                    3 -> binding.resultHeader.text = getString(R.string.hexadecimal_header)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+    }
+
+    private fun convert() {
+        val input = binding.editTextEntering.text.toString().trim()
+        val result = binding.textViewResult
+        try {
+            val from = binding.spinnerFrom.selectedItemPosition
+            val to = binding.spinnerTo.selectedItemPosition
+
+            when (from) {
+                0 -> when (to) {
+                    0 -> result.text = input
+                    1 -> result.text = converter.binaryToDecimal(input).toString()
+                    2 -> result.text = converter.binaryToOctal(input)
+                    3 -> result.text = converter.binaryToHexadecimal(input)
+                }
+                1 ->
+                    when (to) {
+                        0 -> result.text = converter.decimalToBinary(input.toLong())
+                        1 -> result.text = input
+                        2 -> result.text = converter.decimalToOctal(input.toLong())
+                        3 -> result.text = converter.decimalToHexadecimal(input.toLong())
+                    }
+                2 -> when (to) {
+                    0 -> result.text = converter.octalToBinary(input)
+                    1 -> result.text = converter.octalToDecimal(input).toString()
+                    2 -> result.text = input
+                    3 -> result.text = converter.octalToHexadecimal(input)
+                }
+                3 -> when (to) {
+                    0 -> result.text = converter.hexadecimalToBinary(input)
+                    1 -> result.text = converter.hexadecimalToDecimal(input).toString()
+                    2 -> result.text = converter.hexadecimalToOctal(input)
+                    3 -> result.text = input
+                }
+            }
+        } catch (e: Exception) {
+            result.text = getString(R.string.invalid)
+        }
+    }
+}
